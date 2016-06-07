eabfa8a9bb6ee198f77748bae188d189cb1b955f
agusti <agusti.moll@guifi.net>
2016-06-07 20:52:47 +0200

    Build random number

--- src/iban_generator.js
+++ src/iban_generator.js
@@ -1,8 +1,24 @@
 'use strict'
 
 var iban = require('iban');
-const EventEmitter = require('events').EventEmitter;
 
-class ibanGenerator extends EventEmitter{
+function ibanGenerator() {
 
 }
+
+ibanGenerator.prototype.randomNumber = function(){
+  var entitat = this._randomNumber(4);
+  var oficina = this._randomNumber(4);
+  var compte = this._randomNumber(10);
+  return entitat+oficina+compte;
+}
+ibanGenerator.prototype._randomNumber = function(n){
+  var num = "";
+  for(let index=0; index < n; index++ ){
+    num += Math.floor((Math.random() * 10) + 1);
+  }
+  return num;
+}
+
+
+module.exports = ibanGenerator;
