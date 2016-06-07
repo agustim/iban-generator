6d50d08c9f9c6d9a6418718649db46925b741244
agusti <agusti.moll@guifi.net>
2016-06-07 20:52:34 +0200

    Change in test format

--- test/iban_generator_001.js
+++ test/iban_generator_001.js
@@ -3,26 +3,29 @@ var ibanGenerator = require('../src/iban_generator.js');
 
 
 describe('Testing de ibanGenerator:', function(){
+  before(function(){
+    ig = new ibanGenerator();
+  })
   it('Generar numeros aleatoris compte', function(){
-    expect(ibanGenerator.randomNumber()).toMatch(/\d{1,}/);
+    expect(ig.randomNumber()).toMatch(/\d{1,}/);
   })
   it('Digit control CCC', function(){
     var ccc = ""
     var dc = ""
-    expect(ibanGenerator.controlDigit(ccc)).toBe(dc);
+    expect(ig.controlDigit(ccc)).toBe(dc);
   })
   it('Calcular IBAN', function(){
     var ccc = "";
     var iban = "";
-    expect(ibanGenerator.doIban(ccc)).toBe(iban);
+    expect(ig.doIban(ccc)).toBe(iban);
   })
   it('Validar IBAN correcte', function(){
     var iban = '';
-    expect(ibanGenerator.checkIban(iban)).toBe(true);
+    expect(ig.checkIban(iban)).toBe(true);
   })
   it('Validar IBAN incorrecte', function(){
     var iban = '';
-    expect(ibanGenerator.checkIban(iban)).toBe(true);
+    expect(ig.checkIban(iban)).toBe(true);
   })
 
 
